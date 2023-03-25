cfa6785b212edfec0e0b08cd952805801bb8e204
marta <janiuk.marta@gmail.com>
2023-03-25 18:29:34 +0100

    dzien 3

--- src/main/java/pl/coderslab/repository/BookRepository.java
+++ src/main/java/pl/coderslab/repository/BookRepository.java
@@ -3,6 +3,7 @@ package pl.coderslab.repository;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.coderslab.entity.Book;
 
 import java.util.List;
@@ -18,8 +19,12 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findAll();
 
     @Override
+
     @EntityGraph(attributePaths = {"publisher", "authors"}) //wygenruje automatycznie złączenie, do edycji
-    Optional<Book> findById(Long id);
+    //@Query("select distinct b from Book b left join fetch b.publisher left join fetch b.authors")
+    ////dla sposobu z Query trzeba dodać @Param ("id"), dla @EntityGraph wystarczy (Long id)
+    //Optional<Book> findById(@Param ("id") Long id);
+    Optional<Book> findById( Long id);
 
 
 }
